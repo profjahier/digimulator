299cbe084e1c35562f59fbc8fa066c1452666964
Ronan Jahier <ronan.jahier@orange.fr>
2019-12-28 10:06:24 +0100

    ajout d2h()

--- mes_outils.py
+++ mes_outils.py
@@ -22,3 +22,9 @@ def d2b(d):
      sortie : chaine de 8 caractères ('0' ou '1')  """
     return bin(d)[2:].rjust(8, '0')
 
+
+def d2h(d):
+    """ convertit un entier en une chaine de 2 caractères hexadécimaux 
+     entrée : d = nb entier (base décimale)
+     sortie : chaine de 2 caractères ('0' à 'F')  """
+    return hex(d)[2:].rjust(2, '0')
