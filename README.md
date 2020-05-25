8eb5e1b8fd7a34e5ddc5bb6a753c928c38648649
Olivier Lecluse <olivier.lecluse@free.fr>
2020-05-25 18:41:35 +0200

    add new version of pov_smiley_face.asm using inderect adressing

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # digimulator
-Simulator of digirule 2A (https://bradsprojects.com/digirule2/) written in Python (by roro &amp; wawa)
+Simulator of **digirule 2** (https://bradsprojects.com/digirule2/) written in Python (by roro &amp; wawa)
+
+# Instruction set
+
+Digimulator offers the *Digirule 2B* enhanced instruction set. See https://github.com/wawachief/DGR2B for more informations.
 
 <img src="licence.png" alt="licence CC-BY-NC-SA" height="30"/>
--- sources/pov_smiley_face.asm
+++ sources/pov_smiley_face.asm
@@ -2,56 +2,33 @@
 // the information to draw an 8x8 smiley face. The information is sent to the
 // 8 data LED's and when the Digirule is waved in the air (in a dark - or 
 // semi-dark room) you will see a smiley face been drawn in the air.
-// NOTE: when you run the program, you will need to turn the address LED's off
-// by pressing the 'goto' button.
+
+//
+// This version uses the Digirule 2B instruction set and uses indirect adressing.
+//
 
 // Constants
-%define numberToAddToProgramCounter 240
-%define statusRegister      252
-%define buttonRegister      253
-%define addressLEDRegister  254
-%define dataLEDRegister     255
+%define index           240
+%define lineadr         241
+%define output          242
+%define statusRegister  252
+%define dataLEDRegister 255
+%define hideAddressBit  2
 
-// Setup (this code runs once)
-initsp
-speed 1
-copylr 0 numberToAddToProgramCounter
+	initsp
+	speed 1
+	copylr dataLEDRegister output
+	sbr hideAddressBit statusRegister
+:mainloop
+	copylr 8 index
+	copylr POV lineadr
+:loop
+	copyii lineadr output
+	incr lineadr
+	decrjz index
+	jump loop
+	jump mainloop
 
-// Main loop (this code runs repeatedly until power is removed) 
-:Loop
-call SmileyFaceData
-copyar dataLEDRegister
-copyra numberToAddToProgramCounter
-addla 2
-copyar numberToAddToProgramCounter
-jump loop
 
-// Padding
-nop
-nop
-nop
-nop
-nop
-nop
-nop
-nop
-nop
-nop
-nop
-nop
-nop
-nop
-
-:SmileyFaceData
-addrpc numberToAddToProgramCounter
-retla 126
-retla 129
-retla 165
-retla 129
-retla 165
-retla 153
-retla 129
-retla 126
-nop
-copylr 0 numberToAddToProgramCounter
-jump SmileyFaceData    
+// Drawing
+%data POV 126 129 165 129 165 153 129 126
