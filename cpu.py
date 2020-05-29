5b69f271f37d08eafa39d9385b6b31111b94ac69
Olivier Lecluse <olivier.lecluse@free.fr>
2020-05-29 13:17:29 +0200

    menage

--- cpu.py
+++ cpu.py
@@ -18,7 +18,31 @@ class Cpu:
         self.opstack = [0]*OPSTACK_DEPTH
         self.SP = 0
         self.OSP = 0
+    
+    def b2d(b):
+        """ convertit une chaine de 8 bits en nb décimal 
+        entrée : b = chaine de 8 caractères ('0' ou '1') 
+        sortie : d = nb entier (base décimale) """
+        d = 0
+        for i,n in enumerate(b):
+            d += 2**(7-i) * int(n)
+        return d
+        
+    def d2b(d):
+        """ convertit un entier en une chaine de 8 bits 
+        entrée : d = nb entier (base décimale)
+        sortie : chaine de 8 caractères ('0' ou '1')  """
+        return bin(d)[2:].rjust(8, '0')
 
+
+    def d2h(d, mode):
+        """ convertit un entier en une chaine de 2 caractères hexadécimaux 
+        entrée : d = nb entier (base décimale)
+        sortie : chaine de 2 caractères ('0' à 'F')  """
+        if mode==1:
+            return hex(d)[2:].rjust(2, '0')
+        else:
+            return str(d).rjust(3, '0')
     #
     # Main code (digirule)
     #
