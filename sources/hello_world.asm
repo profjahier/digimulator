192a5551a30028df940de0c645bdb25799e12bfc
Olivier Lecluse <olivier.lecluse@free.fr>
2020-06-02 17:49:27 +0200

    POC microbit examples

---
+++ sources/hello_world.asm
@@ -0,0 +1,85 @@
+%define statusReg 0b11111100
+%define zeroFlag  0
+
+speed 128
+initsp
+copylr 0 index
+:loop
+    copyla hw
+    addra index
+    copyar cpy_from
+    call _copyia
+    bcrsc zeroFlag statusReg
+    jump fin
+    comout
+    incr index
+    jump loop
+:fin
+    halt
+    
+
+// Indirect copy : COPYIA - DGR2A compatible
+%data _copyia 6
+%data cpy_from 0
+return 
+
+// data zone
+%data index 0
+%data hw 72 101 108 108 111 44 32 119 111 114 108 100 33 0
+
+#
+# version qui marche sur DGR !!
+#
+%define statusReg 0b11111100
+%define zeroFlag  0
+
+speed 128
+initsp
+copylr 0 index
+copylr res cpy_to
+:loop
+    copyla hw
+    addra index
+    copyar cpy_from
+    call _copyii
+    bcrsc zeroFlag statusReg
+    jump fin
+    copyra res
+    comout
+    incr index
+    jump loop
+:fin
+    halt
+    
+
+// Indirect copy : COPYII - DGR2A compatible
+%data _copyii 7
+%data cpy_from 0
+%data cpy_to 0
+return 
+
+// data zone
+%data index 0
+%data res 0
+%data hw 72 101 108 108 111 44 32 119 111 114 108 100 33 0
+
+##
+## Isolation du bug
+##
+
+copyla test 
+addla 1
+copyar cpy_from
+call _copyia
+copyar 255
+:wait
+    jump wait
+
+// Indirect copy : COPYIA - DGR2A compatible
+%data _copyia 6
+%data cpy_from 0
+return 
+
+%data test 65 66
+
+# Affichage attendu : 66
---
+++ sources/mb_temp.asm
@@ -0,0 +1,7 @@
+:loop
+    comin
+    copyar 255
+    addla 0
+    bcrss 0 252
+    jump loop
+    halt
