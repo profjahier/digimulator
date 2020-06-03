43a46c5af4024bda1ab84834e9813a367ef400c4
Olivier Lecluse <olivier.lecluse@free.fr>
2020-06-03 20:41:03 +0200

    cxfreeze

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ from cx_Freeze import setup, Executable
 # Dependencies are automatically detected, but it might need fine tuning.
 build_exe_options = {
     "copy_dependent_files": True,
-    "packages": ["os", "tkinter", "serial"]
-    "include_files": ["config.ini", "flash_memory.txt"]
+    "packages": ["os", "tkinter", "serial"],
+    "include_files": ["config.ini", "flash_memory.txt", "instructionset_2A.py", "instructionset_2B.py", "instructionset_2U.py"],
     "excludes": []
 }
 
